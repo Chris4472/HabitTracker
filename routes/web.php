ef07a8059baedf09f35e2df8615d54b1547d0ba1
Chris4472 <chrisdawsondeveloper@gmail.com>
2023-03-16 19:15:24 +0000

    Setup Calendar Page
    Setup calendar page
    Added route for calendar
    Updated navigation bar

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/calendar', function () {
+    return Inertia::render('Calendar');
+})->middleware(['auth', 'verified'])->name('calendar');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
